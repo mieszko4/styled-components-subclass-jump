89954bd64caf8959d55c411479d41d8b53fbf5ae
mieszko4 <mieszko4@gmail.com>
2020-08-29 19:26:37 +0200

    Use font locally
    This will cause the jump

--- components/GlobalStyle.js
+++ components/GlobalStyle.js
@@ -4,7 +4,8 @@ export const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: 'Gelion';
     font-weight: 100;
-    src: url('https://stage.kidztopros.com/next-public/fonts/Gelion-Thin.woff2');
+    /* this must be local src in order for the jump to appear */
+    src: url('/Gelion-Thin.woff2');
   }
 
   html, button {
